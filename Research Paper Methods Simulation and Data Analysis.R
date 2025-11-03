309b5d91e0119abd09564eea714197bb2861eca5
Junting Jiang <st.grantjiang@gmail.com>
2025-11-03 17:48:03 -0500

    Graphing and statistics are added

--- Research Paper Methods Simulation and Data Analysis.R
+++ Research Paper Methods Simulation and Data Analysis.R
@@ -1,15 +1,7 @@
 #simulate data
 
 #Create a list of data to dissect, imaging this is a cell with ~100pF cellular capacitance
-<<<<<<< HEAD
-data_list <- lapply(1:10, function(i) {
-=======
-<<<<<<< HEAD
-data_list <- lapply(1:3, function(i) {
-=======
-data_list <- lapply(1:5, function(i) {
->>>>>>> 28207e516b8328a8a4a0f1db9a218899e1f14978
->>>>>>> b2e1f11b61fd5ea9c778b370f234c802df71c512
+data_list <- lapply(1:200, function(i) {
 t<- seq(0, 15000, by = 1) # time in ms
 y_no_noise <- 1800*exp(-t/60) + 1500*exp(-t/800) + 1300*exp(-t/4000) + 400 # simulate a 3-exponential decay with errors
 error_sd <- sqrt(y_no_noise) #poission error
@@ -17,14 +9,6 @@ y <- y_no_noise + rnorm(length(t), 0, sd=error_sd)
 data.frame(t = t, y = y)
 })
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-max_current <- max(y) #for determine current upper boundary
-max_tau <- max(t)# for determine tau upper boundary
->>>>>>> 28207e516b8328a8a4a0f1db9a218899e1f14978
->>>>>>> b2e1f11b61fd5ea9c778b370f234c802df71c512
 
 library(minpack.lm)
 library(ggplot2)
@@ -32,10 +16,9 @@ library(tidyr)
 nlc <- nls.control(maxiter = 1000)
 
 #create a list for results from each loop
-result_list <- list()
-result_list[["2exp"]] <- list()
-result_list[["3exp"]] <- list()
-result_list[["4exp"]] <- list()
+result_2exp <- list()
+result_3exp <- list()
+result_4exp <- list()
 
 
 for (i in 1:length(data_list)) {
@@ -91,8 +74,8 @@ legend("topright", legend=c("Data","Total Fit","τ1","τ2","Residule","Steady-st
        col=c("black","red","blue","green","orange","yellow"), lty=1)
 
 #write parameters from 2-exp fit into the result list
-result_list[["2exp"]][[i]] <- list()
-result_list[['2exp']][[i]]<- data.frame(
+result_2exp[[i]] <- list()
+result_2exp[[i]]<- data.frame(
   ID = paste0("Fit_", i,"-2_exp"),
   Amp1 = A1,
   Amp2 = A2,
@@ -151,8 +134,8 @@ legend("topright", legend=c("Data","Total Fit","τ1","τ2","τ3","Residule","Ste
        col=c("black","red","blue","green","purple","orange","yellow"), lty=1)
 
 #write parameters from 3-exp fit into the result list
-result_list[["3exp"]][[i]] <- list()
-result_list[["3exp"]][[i]]<- data.frame(
+result_3exp[[i]] <- list()
+result_3exp[[i]]<- data.frame(
   ID = paste0("Fit_", i,"-3_exp"),
   Amp1 = A1,
   Amp2 = A2,
@@ -220,8 +203,8 @@ legend("topright", legend=c("Data","Total Fit","τ1","τ2","τ3","τ4", "Residul
        col=c("black","red","blue","green","purple","cyan","orange","yellow"), lty=1)
 
 #write parameters from 4-exp fit into the result list
-result_list[["4exp"]][[i]] <- list()
-result_list[["4exp"]][[i]]<- data.frame(
+result_4exp[[i]] <- list()
+result_4exp[[i]]<- data.frame(
   ID = paste0("Fit_", i,"-4_exp"),
   Amp1 = A1,
   Amp2 = A2,
@@ -239,17 +222,81 @@ result_list[["4exp"]][[i]]<- data.frame(
 
 #analyze listed results
 
-data_names <- c("2-exponential", "3-Exponential", "4-Exponential")
-rc2m <- sapply(result_list$`2exp`, function(x) x$R2)
-rc3m <- sapply(result_list$`3exp`, function(x) x$R2)
-rc4m_pre <- sapply(result_list$`4exp`, function(x) x$R2) #before correct length
-rc4m <- c(rc4m_pre, rep(NA, length(rc3m) - length(rc4m_pre)))
-r_values <- c(rc2m, rc3m, rc4m) 
-table <- data.frame("2exp"=rc2m, "3exp"=rc3m, "4exp"=rc4m)
+result_4exp <- Filter(Negate(is.null), result_4exp) #remove Null results
+result_2exp <- result_2exp[1:105] #correct length
+result_3exp <- result_3exp[1:105] #correct length
+result_4exp <- result_4exp[1:105] #correct length
 
-df_long <- pivot_longer(table, cols = everything(), names_to = "Exponential", values_to = "Value")
-ggplot(df_long, aes(x = Exponential, y = Value, color = Exponential)) +
-  geom_jitter(width = 0.1, size = 3, alpha = 0.8) +
+#First we analyze the correlation coefficient R
+
+r2 <- sapply(result_2exp, function(x) x$R2)
+r3 <- sapply(result_3exp, function(x) x$R2)
+r4 <- sapply(result_4exp, function(x) x$R2)
+r_table <- data.frame("2exp"=r2, "3exp"=r3, "4exp"=r4, check.names = FALSE) #check.name remove annoying "X" in front of any titles start with a number
+
+r_list <- pivot_longer(r_table, cols = everything(), names_to = "Exponential", values_to = "R")
+ggplot(r_list, aes(x = Exponential, y = R)) +
+  geom_jitter(width = 0.05, size = 1.5, alpha = 0.5) +
   theme_minimal() +
-  labs(x = "Exponential Term", y = "Value", title = "Scatter points for each exponential") +
+  labs(y = "Correlation Coefficient (R)", title = "Fitting Quality for Each Exponential Model") +
+  theme(legend.position = "none")
+
+summary(aov(R ~ Exponential, data = r_list))
+TukeyHSD(aov(R ~ Exponential, data = r_list))
+
+#Then the Reduced chi-squared
+
+rcs2 <- sapply(result_2exp, function(x) x$red_chi_2exp)
+rcs3 <- sapply(result_3exp, function(x) x$red_chi_3exp)
+rcs4 <- sapply(result_4exp, function(x) x$red_chi_4exp)
+rcs_table <- data.frame("2exp"=rcs2, "3exp"=rcs3, "4exp"=rcs4, check.names = FALSE) #check.name remove annoying "X" in front of any titles start with a number
+
+rcs_list <- pivot_longer(rcs_table, cols = everything(), names_to = "Exponential", values_to = "RCS")
+ggplot(rcs_list, aes(x = Exponential, y = RCS)) +
+  ylim(0, 3) +
+  geom_jitter(width = 0.05, size = 1.5, alpha = 0.5) +
+  theme_minimal() +
+  labs(y = "Reduced Chi-squared X", title = "Fitting Quality for Each Exponential Model") +
+  theme(legend.position = "none")
+
+summary(aov(RCS ~ Exponential, data = rcs_list))
+TukeyHSD(aov(RCS ~ Exponential, data = rcs_list))
+
+#F-distribution test is a good way to test the necessity of adding an extra component
+
+SSE_2 <- as.numeric(sapply(result_2exp, function(x) x$SSE_2exp))
+SSE_3 <- as.numeric(sapply(result_3exp, function(x) x$SSE_3exp))
+SSE_4 <- as.numeric(sapply(result_4exp, function(x) x$SSE_4exp))
+k_2 <- 5
+k_3 <- 7
+k_4 <- 9
+n <- length(t)
+
+T_2to3 <- ((SSE_2 - SSE_3) / SSE_3) * ((n - k_3) / 2)
+T_3to4 <- ((SSE_3 - SSE_4) / SSE_4) * ((n - k_4) / 3)
+T_table <- data.frame("2-3exp"=T_2to3, "3-4exp"=T_3to4, check.names = FALSE)
+
+T_list <- pivot_longer(T_table, cols = everything(), names_to = "Exponential", values_to = "TV")
+ggplot(T_list, aes(x = Exponential, y = TV)) +
+  geom_jitter(width = 0.05, size = 1.5, alpha = 0.5) +
+  theme_minimal() +
+  labs(y = "T Value", title = "Fitting Quality for Each Exponential Model") +
+  theme(legend.position = "none")
+
+summary(aov(TV ~ Exponential, data = T_list))
+TukeyHSD(aov(TV ~ Exponential, data = T_list))
+
+#Finally, we plot the dissected Tau from each exponential fits
+
+t4 <- c(sapply(result_4exp, function(x) x$Tau1), sapply(result_4exp, function(x) x$Tau2), sapply(result_4exp, function(x) x$Tau3), sapply(result_4exp, function(x) x$Tau4))
+t3 <- c(sapply(result_4exp, function(x) x$Tau1), sapply(result_4exp, function(x) x$Tau2), sapply(result_4exp, function(x) x$Tau3), rep((-1), (length(t4)/4)))
+t2 <- c(sapply(result_4exp, function(x) x$Tau1), sapply(result_4exp, function(x) x$Tau2), rep((-1), (length(t4)/4*2)))
+t_table <- data.frame("2exp"=t2, "3exp"=t3, "4exp"=t4, check.names = FALSE) #check.name remove annoying "X" in front of any titles start with a number
+
+t_list <- pivot_longer(t_table, cols = everything(), names_to = "Exponential", values_to = "Tau")
+ggplot(t_list, aes(x = Exponential, y = Tau)) +
+  scale_y_log10() +
+  geom_jitter(width = 0.05, size = 1.5, alpha = 0.5) +
+  theme_minimal() +
+  labs(y = "Inactivation Time constant Tau (ms)", title = "Fitting Quality for Each Exponential Model") +
   theme(legend.position = "none")
